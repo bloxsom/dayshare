2b080d5a76c7e8d910703ddcb2d5be49d3f7e471
Brandon Bloxsom <bloxsom@umich.edu>
2014-10-20 15:32:40 -0400

    Use Calendar app for free times

--- DayShare/DaySelectViewController.h
+++ DayShare/DaySelectViewController.h
@@ -1,12 +1,10 @@
 #import <UIKit/UIKit.h>
-#import "GADGoogleOAuth.h"
+#import <EventKit/EventKit.h>
+#import <EventKitUI/EventKitUI.h>
 #import "CWStatusBarNotification.h"
 
-@interface DaySelectViewController : UIViewController <UITableViewDelegate, UITableViewDataSource, GoogleOAuthDelegate>
-@property (weak, nonatomic) IBOutlet UIToolbar *toolBar;
+@interface DaySelectViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
-@property (weak, nonatomic) IBOutlet UIBarButtonItem *profileButton;
-@property (weak, nonatomic) IBOutlet UIBarButtonItem *revokeAccessButton;
 
 @property (nonatomic, strong) CWStatusBarNotification *notification;
 @property (nonatomic, strong) NSMutableArray *arrDays;
@@ -15,14 +13,10 @@
 
 @property (nonatomic, strong) NSMutableArray *arrCalendarIds;
 
-@property (nonatomic, strong) GADGoogleOAuth *googleOAuth;
-
 @property(nonatomic) NSString *calendarID;
 @property(nonatomic) NSString *timeStart;
 @property(nonatomic) NSString *timeEnd;
 @property(nonatomic) NSString *timeZone;
-
-- (void)calculateFreeTime:(NSMutableArray *)startDates end:(NSMutableArray *)endDates;
 
 - (void)copyToClipboard:(NSString *)str;
 
